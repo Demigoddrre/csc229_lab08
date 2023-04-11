12c31b9b8373660958718ca407731337286b4866
dandr <dandr@137.125.134.22>
2023-04-10 22:59:29 -0400

    updated linear search analysis

---
+++ src/main/java/lab08/csc229_lab08/SinglyLinkedList.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package lab08.csc229_lab08;
+
+/**
+ *
+ * @author dandr
+ */
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        next = null;
+    }
+}
+
+public class SinglyLinkedList {
+    Node head;
+
+   public SinglyLinkedList() {
+        head = null;
+    }
+
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node currentNode = head;
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+        }
+    }
+
+    public void printList() {
+        Node currentNode = head;
+        while (currentNode != null) {
+            System.out.print(currentNode.data + " ");
+            currentNode = currentNode.next;
+        }
+        System.out.println();
+    }
+
+    public int getSum() {
+        Node currentNode = head;
+        int sum = 0;
+        while (currentNode != null) {
+            sum += currentNode.data;
+            currentNode = currentNode.next;
+        }
+        return sum;
+    }
+}
+
